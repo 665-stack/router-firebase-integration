e3c5340104d8ae5ff8eddfc348cc0571e71f541a
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-10-12 15:07:59 +0600

    fix style

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -3,7 +3,16 @@ import './Login.css'
 const Login = () => {
     return (
         <div className='login'>
-            <h3>Please Login</h3>
+            <div>
+                <h3 className='headline'>Please Login</h3>
+                <form >
+                    <input type="email" placeholder='Your Email' />
+                    <br />
+                    <input type="password" name="" id="" placeholder='password' />
+                    <br />
+                    <input className='submit-btn' type="submit" value="Register" />
+                </form>
+            </div>
         </div>
     );
 };
