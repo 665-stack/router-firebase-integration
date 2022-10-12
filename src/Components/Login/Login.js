252cae08fa519ce300311a4ee845ea5b9e0a6499
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-10-12 15:52:00 +0600

    Create Use Firebase hook for shared authentication

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,12 +5,21 @@ const Login = () => {
         <div className='login'>
             <div>
                 <h3 className='headline'>Please Login</h3>
+
+                <button className='thirdPartyLogin'>Google</button>
+                <button className='thirdPartyLogin'>Facebook</button>
+                <button className='thirdPartyLogin'>GitHub</button>
+
+                <br />
+
+                <span className='or-Btn'>or</span>
+
                 <form >
                     <input type="email" placeholder='Your Email' />
                     <br />
                     <input type="password" name="" id="" placeholder='password' />
                     <br />
-                    <input className='submit-btn' type="submit" value="Register" />
+                    <input className='submit-btn' type="submit" value="Login" />
                 </form>
             </div>
         </div>
---
+++ src/Hooks/useFirebase.js
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+const useFirebase = () => {
+    const [user, setUser] = useState({});
+
+    const signInWithGoogle = () => {
+
+        console.log('sign in soons');
+
+    }
+}
+
+export default useFirebase;
