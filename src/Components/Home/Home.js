1bfb0cf08f0ae16d8d898ef963a94f4537da141a
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-10-12 23:04:01 +0600

    add userName in homepage when user do login

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import useFirebase from '../../Hooks/useFirebase';
 import './Home.css'
 const Home = () => {
+    const { user } = useFirebase();
     return (
         <div className='home'>
-            <h1 className='tagline'>Tracking system for <span>economy</span> of vehicle use and driver behavior</h1>
-            <p>Reduces costs, increases safety</p>
-            <div className="buttons">
-                <button className='readmore-button'>Read more</button>
-                <button className='pdf-button'>Download pdf</button>
+            <div>
+                <h4 className='user-welcome'>Hi, {user ? user.displayName : "there!"}</h4>
+
+                <h1 className='tagline'>Tracking system for <span>economy</span> of vehicle use and driver behavior</h1>
+                <p>Reduces costs, increases safety</p>
+                <div className="buttons">
+                    <button className='readmore-button'>Read more</button>
+                    <button className='pdf-button'>Download pdf</button>
+                </div>
             </div>
         </div>
     );
